8a0b1fdddc0387a5026f4fe07f37adce8f54da7d
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2026-02-04 11:29:57 +0100

    fix(editor): Simplify InputTriple component by removing N8nResizeObserver and restructuring template

--- packages/frontend/editor-ui/src/features/ndv/parameters/components/InputTriple/InputTriple.vue
+++ packages/frontend/editor-ui/src/features/ndv/parameters/components/InputTriple/InputTriple.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { N8nResizeObserver } from '@n8n/design-system';
-
 type Props = {
 	middleWidth?: string;
 };
@@ -8,81 +6,46 @@ withDefaults(defineProps<Props>(), { middleWidth: '160px' });
 </script>
 
 <template>
-	<N8nResizeObserver
-		:class="{ [$style.observer]: true }"
-		:breakpoints="[
-			{ bp: 'stacked', width: 400 },
-			{ bp: 'medium', width: 680 },
-		]"
-	>
-		<template #default="{ bp }">
-			<div :class="$style.background"></div>
-			<div
-				:class="{
-					[$style.triple]: true,
-					[$style.stacked]: bp === 'stacked',
-					[$style.medium]: bp === 'medium',
-					[$style.default]: bp === 'default',
-					[$style.noRightSlot]: !$slots.right,
-					[$style.noMiddleSlot]: !$slots.middle,
-				}"
-			>
-				<div v-if="$slots.left" :class="$style.item">
-					<slot name="left" :breakpoint="bp"></slot>
-				</div>
-				<div
-					v-if="$slots.middle"
-					:class="[$style.item, $style.middle]"
-					:style="{ flexBasis: middleWidth }"
-				>
-					<slot name="middle" :breakpoint="bp"></slot>
-				</div>
-				<div v-if="$slots.right" :class="$style.item">
-					<slot name="right" :breakpoint="bp"></slot>
-				</div>
+	<div :class="$style.container">
+		<div :class="$style.items">
+			<div v-if="$slots.left" :class="[$style.item, $style.itemFirst]">
+				<slot name="left"></slot>
 			</div>
-		</template>
-	</N8nResizeObserver>
+			<div
+				v-if="$slots.middle"
+				:class="[$style.item, $style.itemMiddle]"
+				:style="{ '--middle-width': middleWidth }"
+			>
+				<slot name="middle"></slot>
+			</div>
+			<div v-if="$slots.right" :class="[$style.item, $style.itemLast]">
+				<slot name="right"></slot>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style lang="scss" module>
-.triple {
-	display: flex;
-	flex-wrap: nowrap;
-	align-items: flex-start;
-}
-
-.observer {
+.container {
 	--parameter-input-options--height: 22px;
+	container-type: inline-size;
 	width: 100%;
-	position: relative;
 }
 
-.background {
-	position: absolute;
-	background-color: var(--input-triple--color--background);
-	top: var(--parameter-input-options--height);
-	bottom: 0;
-	left: 0;
-	right: 0;
-	border: 1px solid var(--border-color);
-	border-radius: var(--radius);
+.items {
+	display: flex;
+	flex-wrap: wrap;
 }
 
 .item {
 	position: relative;
-	flex-shrink: 0;
-	flex-basis: 240px;
-	flex-grow: 1;
-	z-index: 0;
+	flex: 1;
+	min-width: 0;
 
-	--input--radius: 0;
+	input {
+		box-sizing: content-box;
+	}
 
-	// Suppress N8nInput's border since .background div handles it
-	--input--border-color: transparent;
-	--input--border-width: 0;
-
-	// Remove gap that breaks visual unity with .background div
 	:global(.n8n-input) {
 		gap: 0;
 	}
@@ -92,84 +55,52 @@ withDefaults(defineProps<Props>(), { middleWidth: '160px' });
 	}
 }
 
-.default .item:not(:first-child) {
-	margin-left: -1px;
-}
-
-.middle {
-	flex-grow: 0;
+.itemMiddle {
+	margin: 0 -1px;
 	flex-basis: 160px;
+	flex-grow: 0;
 }
 
-.item:first-of-type {
-	--input--radius--top-left: var(--radius);
-	--input--radius--bottom-left: var(--radius);
-	--input-triple--radius--top-right: 0;
-	--input-triple--radius--bottom-right: 0;
-}
-
-.item:last-of-type {
-	--input--radius--top-left: 0;
-	--input--radius--bottom-left: 0;
-	--input-triple--radius--top-right: var(--radius);
-	--input-triple--radius--bottom-right: var(--radius);
-}
-
-.medium:not(.noRightSlot) {
-	flex-wrap: wrap;
-
-	.middle {
-		margin-left: -1px;
-
-		--input-triple--radius--top-right: var(--radius);
-		--input-triple--radius--bottom-right: 0;
+.itemFirst {
+	input {
+		border-top-right-radius: 0;
+		border-bottom-right-radius: 0;
 	}
+}
 
-	.item:first-of-type {
-		--input--radius--top-left: var(--radius);
-		--input-triple--radius--top-right: 0;
-		--input--radius--bottom-left: 0;
+.itemLast {
+	input {
+		border-top-left-radius: 0;
+		border-bottom-left-radius: 0;
 	}
+}
 
-	.item:last-of-type {
-		flex-basis: 400px;
+// Stacked layout when container is narrow
+@container (max-width: 400px) {
+	.item {
+		flex-basis: 100%;
+		margin: 0;
 		margin-top: -1px;
 
-		--input--radius--top-left: 0;
-		--input-triple--radius--top-right: 0;
-		--input--radius--bottom-left: var(--radius);
-		--input-triple--radius--bottom-right: var(--radius);
-	}
-}
-
-.stacked {
-	display: block;
-
-	.middle {
-		padding-top: 0;
+		&:first-child {
+			margin-top: 0;
+		}
 	}
 
-	.middle:not(.item:last-of-type) {
-		width: 100%;
-		--input--radius: 0;
+	.itemMiddle {
+		flex-basis: 100%;
 	}
 
-	.item:first-of-type {
-		--input--radius--top-left: var(--radius);
-		--input-triple--radius--top-right: var(--radius);
-		--input--radius--bottom-left: 0;
-		--input-triple--radius--bottom-right: 0;
+	.itemFirst input {
+		border-radius: var(--radius) var(--radius) 0 0;
 	}
 
-	.item:not(:first-of-type) {
-		margin-top: -1px;
+	.itemMiddle input {
+		border-radius: 0;
 	}
 
-	.item:last-of-type {
-		--input--radius--top-left: 0;
-		--input-triple--radius--top-right: 0;
-		--input--radius--bottom-left: var(--radius);
-		--input-triple--radius--bottom-right: var(--radius);
+	.itemLast input {
+		border-radius: 0 0 var(--radius) var(--radius);
 	}
 }
 </style>
